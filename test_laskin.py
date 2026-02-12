5b2491aed22f561b684090b87a2c99202b032697
saarinik <167483892+saarinik@users.noreply.github.com>
2026-02-12 09:24:04 +0200

    Fix indentation in test_plus method

--- test_laskin.py
+++ test_laskin.py
@@ -1,9 +1,12 @@
 import unittest
 from laskin import plus
 
+
 class TestLaskin(unittest.TestCase):
-  def test_plus(self):
-    self.assertEqual(plus(1, 1), 2)
+
+    def test_plus(self):
+        self.assertEqual(plus(1, 1), 2)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
